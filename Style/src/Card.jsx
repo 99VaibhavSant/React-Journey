5a8ce31f15da240cc18c279245c71e470cb863e6
99vaibhavsant <99vaibhavsant@gmail.com>
2025-11-08 18:04:27 +0530

    How component styling work in React

---
+++ Style/src/Card.jsx
@@ -0,0 +1,13 @@
+import "./Card.css"
+
+function Card(){
+    return(
+        
+        <div className="card">
+            <h1>My name is Vaibhav</h1>
+            <p>I am a React devloper</p>
+        </div>
+    )
+}
+
+export default Card;
