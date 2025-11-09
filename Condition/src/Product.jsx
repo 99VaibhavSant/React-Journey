7d19adaad6591ff70d7e71e93b3efaec456f0d6d
99vaibhavsant <99vaibhavsant@gmail.com>
2025-11-09 17:18:14 +0530

    Add commit in it

--- Condition/src/Product.jsx
+++ Condition/src/Product.jsx
@@ -1,6 +1,14 @@
 import "./Product.css"
 
 function Product({ Name, Price}) {
+
+// - Created Product component that accepts 'Name' and 'Price' as props.
+// - Implemented conditional rendering using the ternary operator.
+// - If Price is greater than or equal to 1000, a discount (5% of the price) is displayed.
+// - Demonstrates how to store conditional JSX in a variable (discount) for cleaner code.
+// - Added basic CSS styling through 'Product.css'.
+
+
   // let discount = Price >= 1000 ? <p>Discount of 5%</p> : null  
   let discount = Price >= 1000 ? <p>{Price * 5 / 100}</p> : null  
   return (
